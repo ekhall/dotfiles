3d35e89d10e26722255d781ed50eb254a3178109
E. Kevin Hall <hall@absinthe.org>
2026-07-16 06:58:12 -0400

    Added Framework updates

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -14,13 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(adwaita-dark-theme avy cape compat corfu doom-themes
-			doom-tomorrow-night elfeed elfeed-protocol
-			embark-consult exec-path-from-shell god-mode
-			magit marginalia mixed-pitch mood-line notmuch
-			olivetti orderless org-modern
-			rainbow-delimiters vertico vundo)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
